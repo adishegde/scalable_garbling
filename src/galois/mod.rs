6594178521548ddce72ad8d8aeb64fc8aa3461f9
Aditya Shridhar Hegde <adishegde@gmail.com>
2023-03-06 18:34:56 -0500

    Refactor Galois field
    Introduce galois field type which can be used to create field elements.

--- src/galois/mod.rs
+++ src/galois/mod.rs
@@ -1,18 +1,16 @@
 mod bindings;
+use rand::distributions::{Distribution, Uniform};
+use rand::Rng;
 use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
 
-// Represents a Galois field element.
-// Serves as a thin wrapper around the C library.
-// The implemented operations assume that input field elements have the same width.
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub struct GFElement {
-    value: i32,
-    width: u8
+pub struct GF {
+    width: u8,
+    rand_dist: Uniform<u32>,
 }
 
-impl GFElement {
+impl GF {
     // Setup field with specified width by pre-computing necessary data.
-    pub fn setup(width: u8) -> Result<(), &'static str> {
+    pub fn new(width: u8) -> Result<GF, &'static str> {
         if width > 30 {
             return Err("Width is too large.");
         }
@@ -22,17 +20,49 @@ impl GFElement {
         if ret != 0 {
             Err("Could not create log tables.")
         } else {
-            Ok(())
+            Ok(GF {
+                width,
+                rand_dist: Uniform::new(0, 2_u32.pow(width.into())),
+            })
         }
     }
 
-    pub fn get_field(&self) -> u8 {
-        return self.width;
+    // Get field element with value `value`.
+    pub fn get(&self, value: u32) -> GFElement {
+        GFElement {
+            value,
+            width: self.width,
+        }
     }
 
-    pub fn from(value: i32, width: u8) -> Self {
-        GFElement { value, width }
+    // Get a random field element.
+    pub fn rand<R: Rng>(&self, rng: &mut R) -> GFElement {
+        GFElement {
+            value: self.rand_dist.sample(rng),
+            width: self.width,
+        }
     }
+
+    pub fn one(&self) -> GFElement {
+        self.get(1)
+    }
+
+    pub fn zero(&self) -> GFElement {
+        self.get(0)
+    }
+
+    pub fn order(&self) -> u32 {
+        2_u32.pow(self.width as u32)
+    }
+}
+
+// Represents a Galois field element.
+// Serves as a thin wrapper around the C library.
+// The implemented operations assume that input field elements have the same width.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct GFElement {
+    value: u32,
+    width: u8,
 }
 
 impl AddAssign for GFElement {
@@ -41,7 +71,6 @@ impl AddAssign for GFElement {
         self.value ^= rhs.value;
     }
 }
-
 
 impl Add for GFElement {
     type Output = Self;
@@ -75,7 +104,11 @@ impl Sub for GFElement {
 impl MulAssign for GFElement {
     fn mul_assign(&mut self, rhs: Self) {
         self.value = unsafe {
-            bindings::galois_logtable_multiply(self.value, rhs.value, self.width.into())
+            bindings::galois_logtable_multiply(
+                self.value as i32,
+                rhs.value as i32,
+                self.width.into(),
+            ) as u32
         }
     }
 }
@@ -93,7 +126,8 @@ impl Mul for GFElement {
 impl DivAssign for GFElement {
     fn div_assign(&mut self, rhs: Self) {
         self.value = unsafe {
-            bindings::galois_logtable_divide(self.value, rhs.value, self.width.into())
+            bindings::galois_logtable_divide(self.value as i32, rhs.value as i32, self.width.into())
+                as u32
         };
     }
 }
--- tests/galois.rs
+++ tests/galois.rs
@@ -1,102 +1,102 @@
-use scalable_mpc::galois::GFElement;
+use scalable_mpc::galois::GF;
 use serial_test::serial;
 use std::thread;
 
 const GF_WIDTH: u8 = 18;
 
 // Creating a from field is not thread safe and so the tests can't be run in parallel.
-fn setup() {
-    GFElement::setup(GF_WIDTH).unwrap()
+fn setup() -> GF {
+    GF::new(GF_WIDTH).unwrap()
 }
 
 #[test]
 #[serial]
 fn add() {
-    setup();
-    let a = GFElement::from(5, GF_WIDTH);
-    let b = GFElement::from(3, GF_WIDTH);
+    let gf = setup();
+    let a = gf.get(5);
+    let b = gf.get(3);
     let c = a + b;
 
-    assert_eq!(c, GFElement::from(6, GF_WIDTH));
+    assert_eq!(c, gf.get(6));
 }
 
 #[test]
 #[serial]
 fn add_assign() {
-    setup();
-    let mut a = GFElement::from(5, GF_WIDTH);
-    let b = GFElement::from(3, GF_WIDTH);
+    let gf = setup();
+    let mut a = gf.get(5);
+    let b = gf.get(3);
     a += b;
 
-    assert_eq!(a, GFElement::from(6, GF_WIDTH));
+    assert_eq!(a, gf.get(6));
 }
 
 #[test]
 #[serial]
 fn mul() {
-    setup();
-    let a = GFElement::from(7, GF_WIDTH);
-    let b = GFElement::from(9, GF_WIDTH);
+    let gf = setup();
+    let a = gf.get(7);
+    let b = gf.get(9);
     let c = a * b;
 
-    assert_eq!(c, GFElement::from(63, GF_WIDTH));
+    assert_eq!(c, gf.get(63));
 }
 
 #[test]
 #[serial]
 fn mul_assign() {
-    setup();
-    let mut a = GFElement::from(7, GF_WIDTH);
-    let b = GFElement::from(9, GF_WIDTH);
+    let gf = setup();
+    let mut a = gf.get(7);
+    let b = gf.get(9);
     a *= b;
 
-    assert_eq!(a, GFElement::from(63, GF_WIDTH));
+    assert_eq!(a, gf.get(63));
 }
 
 #[test]
 #[serial]
 fn div() {
-    setup();
-    let a = GFElement::from(63, GF_WIDTH);
-    let b = GFElement::from(7, GF_WIDTH);
+    let gf = setup();
+    let a = gf.get(63);
+    let b = gf.get(7);
     let c = a / b;
 
-    assert_eq!(c, GFElement::from(9, GF_WIDTH));
+    assert_eq!(c, gf.get(9));
 }
 
 #[test]
 #[serial]
 fn div_assign() {
-    setup();
-    let mut a = GFElement::from(63, GF_WIDTH);
-    let b = GFElement::from(7, GF_WIDTH);
+    let gf = setup();
+    let mut a = gf.get(63);
+    let b = gf.get(7);
     a /= b;
 
-    assert_eq!(a, GFElement::from(9, GF_WIDTH));
+    assert_eq!(a, gf.get(9));
 }
 
 #[test]
 #[serial]
 fn ops_in_parallel() {
-    setup();
+    let gf = setup();
+    let one = gf.one();
 
-    let one = GFElement::from(1, GF_WIDTH);
+    let prod = thread::scope(|s| {
+        let h1 = s.spawn(|| {
+            let beg = 1;
+            let end = gf.order() / 2;
+            (beg..end).fold(one, |acc, x| acc * gf.get(x))
+        });
 
-    let h1 = thread::spawn(move || {
-        let beg = 1;
-        let end = 2_i32.pow((GF_WIDTH as u32) - 1);
-        (beg..end).fold(one, |acc, x| acc * GFElement::from(x, GF_WIDTH))
+        let h2 = s.spawn(|| {
+            let beg = gf.order() / 2;
+            let end = gf.order();
+            (beg..end).fold(one, |acc, x| acc * gf.get(x))
+        });
+
+        h1.join().unwrap() * h2.join().unwrap()
     });
 
-    let h2 = thread::spawn(move || {
-        let beg = 2_i32.pow((GF_WIDTH as u32) - 1);
-        let end = 2_i32.pow(GF_WIDTH as u32);
-        (beg..end).fold(one, |acc, x| acc * GFElement::from(x, GF_WIDTH))
-    });
-
-    let prod = h1.join().unwrap() * h2.join().unwrap();
-
-    let exp =
-        (1..2_i32.pow(GF_WIDTH as u32)).fold(one, |acc, x| acc * GFElement::from(x, GF_WIDTH));
+    let exp = (1..2_u32.pow(GF_WIDTH as u32)).fold(one, |acc, x| acc * gf.get(x));
     assert_eq!(prod, exp);
 }
